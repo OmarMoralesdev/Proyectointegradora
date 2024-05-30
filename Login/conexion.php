5f8e178fb957c80aa1e4cfd8a9f3c75e9667e1c1
JorgeLIV <23170082@uttcampus.edu.mx>
2024-05-30 11:49:01 -0600

    Cambios generales, actualización

--- Login/conexion.php
+++ Login/conexion.php
@@ -1,15 +1,13 @@
 <?php
 // Datos de conexión a la base de datos
-$servername = "127.0.0.1";
+$servername = "127.0.0.1:3307";
 $db_username = "root"; // Cambia esto por tu nombre de usuario de MySQL
 $db_password = ""; // Cambia esto por tu contraseña de MySQL
-$dbname = "integradoraiglesia";
+$dbname = "taller_euroservice";
 
 // Crear conexión
 $conn = new mysqli($servername, $db_username, $db_password, $dbname);
-
-// Verificar conexión
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die("La conexión ha fallado: " . $conn->connect_error);
 }
 ?>
